1a77148ea48fe9206affb86f1d9ef3d8fc81f183
John Cokos <johncokos@Office-Mac-Mini.local>
2023-08-22 19:42:19 -0700

    hello world

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("I am working");
+});
+
+app.listen( PORT, () => console.log("Running") );
